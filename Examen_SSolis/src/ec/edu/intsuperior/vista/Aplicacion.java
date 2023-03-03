d3f771b4cc34bbe0b82339316a3cf1e5e96e0934
wssolis <sys.solis@gmail.com>
2023-03-02 19:56:11 -0500

    Se creo el metodo para verificar cedula ecuatoriana

--- Examen_SSolis/src/ec/edu/intsuperior/vista/Aplicacion.java
+++ Examen_SSolis/src/ec/edu/intsuperior/vista/Aplicacion.java
@@ -5,13 +5,53 @@
  */
 package ec.edu.intsuperior.vista;
 
+import java.util.Scanner;
+
 /**
  *
  * @author wssa8
  */
 public class Aplicacion {
+
     public static void main(String[] args) {
-        
+        comprobarCedula();
     }
-    
+
+    public static void comprobarCedula() {
+        Scanner leer = new Scanner(System.in);
+        String ci;
+        int dci, acu = 0, aux, resta;
+        System.out.println("Ingresa tu numero de cédula");
+        ci = leer.next();
+        for (int i = 0; i <= 8; i++) {
+            if (i % 2 == 0) {
+                dci = Character.getNumericValue(ci.charAt(i));
+                dci = dci * 2;
+                if (dci >= 10) {
+                    dci = dci - 9;
+                    acu = acu + dci;
+                } else {
+                    acu = acu + dci;
+                }
+            } else {
+                dci = Character.getNumericValue(ci.charAt(i));
+                acu = acu + dci;
+            }
+        }
+
+        if (acu % 10 != 0) {
+            aux = (acu / 10 +1) * 10;
+            resta = aux - acu;
+        }else{
+            aux = acu;
+            resta = aux - acu;
+        }
+        if (Character.getNumericValue(ci.charAt(9)) == resta) {
+            System.out.println("Es una cedula valida ");
+        } else {
+            System.out.println("NO Es una cedula valida ");
+        }
+
+    }
+
 }
